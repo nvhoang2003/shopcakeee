a91c4ecd43d1cc041735e14d493e2f7ffe79483e
admin <s>
2022-07-01 22:59:16 +0700

    add filessss

--- resources/views/Sign/login.blade.php
+++ resources/views/Sign/login.blade.php
@@ -15,6 +15,27 @@
           href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
           integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
           crossorigin="anonymous">
+
+
+    <link href="{{asset('css/font-face.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="{{asset('vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="{{asset('vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/wow/animate.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/slick/slick.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="{{asset('css/theme.css')}}" rel="stylesheet" media="all">
     <style>
         @import url(https://fonts.googleapis.com/css?family=Raleway:300,400,600);
 
@@ -62,10 +83,10 @@
 <body>
 
 
-@include('partial.error')
+
 <main class="login-form">
 
-    <div class="container d-flex flex-column">
+    <div class="container d-flex flex-column" >
         <div class="row vh-100">
             <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
                 <div class="d-table-cell align-middle">
@@ -75,42 +96,37 @@
                             Sign in to your account to continue
                         </p>
                     </div>
-
-
-
-                    <div class="card">
+                <div class="card">
                     <div class="card-body">
                         <div class="m-sm-4">
                             <div class="text-center">
-                                <img src="{{asset('/storage/images/logo/'.'logo.png')}}" alt="" height="132" width="132" class="img-fluid rounded-circle">
+                                <img src="{{asset('/storage/images/logo/'.'logocakeshop.jpg')}}" alt="" height="200" width="200" class="img-fluid rounded-circle">
                             </div>
                         <form
                             action="{{route('auth.signin')}}"
                             method="post">
                             @csrf
-                            <div class="form-group row">
-                                <div class="mb-3">
-                                <label for="username" class="form-label">User Name</label>
-                                    <input type="text" id="username" class="form-control form-control-lg" name="username" value="{{old('username')}}">
-                                </div>
+                            <div class="form-group">
+                                <label label for="username" class="form-label" >User Name </label>
+                                <input class="au-input au-input--full" type="text" class="form-control form-control-lg"
+                                       id = "username" name="username"
+                                       placeholder="Username"
+                                       value="{{old('username')}}">
+                            </div>
+                            <div class="form-group">
+                                <label label for="password" class="form-label" >Password </label>
+                                <input class="au-input au-input--full" type="password" id="password" class="form-control form-control-lg" name="password"
+                                       placeholder="Password"
+                                       >
                             </div>
 
-                            <div class="form-group row">
-                                <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                    <input type="password" id="password" class="form-control form-control-lg" name="password" >
-                                </div>
-                            </div>
-
-                            <div class="text-center mt-3">
-                                <button type="submit" class="btn btn-primary">
-                                    Sign In
-                                </button>
+                            <div class="text-center mt-3 ">
+                                <button class="au-btn au-btn--block au-btn--green m-b-20 " style=" background-color: #533f03" type="submit">sign in</button>
                             </div>
                         </form>
 
                     </div>
-
+                        @include('partial.error')
                 </div>
             </div>
           </div>
@@ -126,5 +142,29 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
         integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
         crossorigin="anonymous"></script>
+
+<script src="{{asset('vendor/jquery-3.2.1.min.js')}}"></script>
+<!-- Bootstrap JS-->
+<script src="{{asset('vendor/bootstrap-4.1/popper.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
+<!-- Vendor JS       -->
+<script src="{{asset('vendor/slick/slick.min.js')}}">
+</script>
+<script src="{{asset('vendor/wow/wow.min.js')}}"></script>
+<script src="{{asset('vendor/animsition/animsition.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}">
+</script>
+<script src="{{asset('vendor/counter-up/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('vendor/counter-up/jquery.counterup.min.js')}}">
+</script>
+<script src="{{asset('vendor/circle-progress/circle-progress.min.js')}}"></script>
+<script src="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+<script src="{{asset('vendor/chartjs/Chart.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/select2/select2.min.js')}}">
+</script>
+
+<!-- Main JS-->
+<script src="{{asset('js/main.js')}}"></script>
+
 </body>
 </html>
